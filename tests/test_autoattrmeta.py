be3e7d2a832c908474ec465620185ccace82809b
Brutus <brutus.dmc@googlemail.com>
2016-10-01 20:33:40 +0200

    refactored the whole module, dumped fields and widgets

---
+++ tests/test_autoattrmeta.py
@@ -0,0 +1,61 @@
+"""
+Tests for the :cls:`wtforms_html5.AutoAttrMeta` class.
+
+"""
+
+from __future__ import absolute_import
+from __future__ import unicode_literals
+from __future__ import print_function
+
+from unittest import TestCase
+
+from wtforms.validators import (
+    InputRequired,
+    Length,
+)
+
+from . import (
+    get_form,
+    get_field,
+)
+
+
+class TestMeta(TestCase):
+
+    def test_standard_field(self):
+        form1 = get_form()
+        form2 = get_form(use_meta=True)
+        self.assertEqual(form1.test_field(), form2.test_field())
+
+    def test_required(self):
+        form = get_form(validators=[InputRequired(), ], use_meta=True)
+        field = get_field(form)
+        self.assertIn('required', field.attrs)
+
+    def test_invalid(self):
+        form = get_form(validators=[InputRequired(), ], use_meta=True)
+        self.assertFalse(form.validate())
+        field = get_field(form)
+        self.assertIn('invalid', field.attrs['class'])
+
+    def test_minmax(self):
+        # min
+        form = get_form(validators=[Length(min=3), ], use_meta=True)
+        field = get_field(form)
+        self.assertEqual(field.attrs['min'], '3')
+        self.assertNotIn('max', field.attrs)
+        # max
+        form = get_form(validators=[Length(max=8), ], use_meta=True)
+        field = get_field(form)
+        self.assertEqual(field.attrs['max'], '8')
+        self.assertNotIn('min', field.attrs)
+        # min + max
+        form = get_form(validators=[Length(min=3, max=8), ], use_meta=True)
+        field = get_field(form)
+        self.assertEqual(field.attrs['min'], '3')
+        self.assertEqual(field.attrs['max'], '8')
+
+    def test_title(self):
+        form = get_form(description='Some help text', use_meta=True)
+        field = get_field(form)
+        self.assertEqual(field.attrs['title'], 'Some help text')
--- wtforms_html5.py
+++ wtforms_html5.py
@@ -1,511 +1,166 @@
+"""
+WTForms HTML5 adds some render keywords to a fields widget based on the
+fields settings.
+
+The :func:`get_html5_kwargs` returns those automatically generated rendering
+keywords for a field. The :cls:`AutoAttrMeta` can be included in your forms to
+handle this automatically for each field of the form.
+
+Example
+=======
+
+Declare your form just like vanilla WTForms but include :cls:`AutoAttrMeta`
+as your meta class::
+
+>>> from wtforms import Form, StringField
+>>> from wtforms.validators import InputRequired, Length
+>>> from wtforms_html5 import AutoAttrMeta
+>>> class MyForm(Form):
+...   class Meta(AutoAttrMeta):
+...     pass
+...   test_field = StringField(
+...     'Testfield',
+...      validators=[InputRequired(), Length(min=3, max=12)],
+...      description='Just a test field.',
+...   )
+>>> form = MyForm()
+
+The only difference is, that you include a `Meta` class that inherits from
+:cls:`AutoAttrMeta`. Now you get some attributes created automatically for your fields:
+
+>>> form.test_field()
+'<input id="test_field" max="12" min="3" name="test_field" required title="Just a test field." type="text" value="">'
+
+As you can see, the *min* and *max* attributes are created because you used
+the `Length` validator. The field also gets a *title* taken from the fields
+`description`. And the field is marked *required* because of the
+`InputRequired` validator.
+
+If you validate the form and any errors pop up, the field would also get an
+*invalid* attribute::
+
+>>> form.validate()
+False
+>>> form.test_field()
+'<input class="invalid" id="test_field" max="12" min="3" name="test_field" required title="Just a test field." type="text" value="">'
+
+"""
+
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-"""
-WTForms HTML5 Widgets
-~~~~~~~~~~~~~~~~~~~~~
-
-This modul adds HTML5 widgets to WTForms_.
-
-It supports the new INPUT **types** for fields and also sets some of the
-new INPUT **attributes** automatically (based on widget type and what kind of
-validators are set for the field).
-
-There are **widgets** for all the new INPUT types, that you can use in your
-own fields and also **field** classes ready to use. Along with some
-**validators** that take advantage of the new attributes.
-
-Use it just like WTForms_. The only difference is, that you import the
-**fields** from ``wtforms_html5`` instead.
-
-
-Examples
-========
-
-First import the needed modules...
-
->>> from wtforms import Form
->>> from wtforms.validators import Length, NumberRange, DataRequired
->>> from wtforms_html5 import TextField, IntegerField, DateField
->>> from wtforms_html5 import DateRange
-
-And some extra stuff for our examples  (nomally not needed)
-
->>> from datetime import date
->>> from werkzeug.utils import MultiDict
-
-Then comes the main part: declare your form. This works just like vanilla
-WTForms, just use the **fields** you imported from ``wtforms_html5``
-instead...
-
->>> class TestForm(Form):
-...  name = TextField('Name', validators=[DataRequired(), Length(5, 25)])
-...  number = IntegerField('Number', validators=[NumberRange(1000, 9999)], description='Some stuff...')
-...  date = DateField('Date:', validators=[DateRange(date(2000,1,1), date(2012,4,20))])
-...
-
-Now let's see, how the generated input fields look like... the ``min``,
-``max``, ``required`` and ``title`` attributes where auto-generated from the
-declaration.
-
->>> f = TestForm()
->>> f.name()
-'<input id="name" max="25" min="5" name="name" required="required" type="text" value="">'
->>> f.number()
-'<input id="number" max="9999" min="1000" name="number" title="Some stuff..." type="number" value="">'
->>> f.date()
-'<input id="date" max="2012-04-20" min="2000-01-01" name="date" type="date" value="">'
-
-And finally some quick tests for ``DateRange`` and the setting of the
-``invalid`` class on error...
-
->>> d = MultiDict({'name':'Testor', 'date':'1995-05-01'})
->>> f.process(d)  # enter the data to the form
->>> f.validate()  # and check for errors...
-False
->>> f.errors
-{'date': ['Date must be >= 2000-01-01.'], 'number': ['Number must be between 1000 and 9999.']}
->>> f.number()
-'<input class="invalid" id="number" max="9999" min="1000" name="number" title="Some stuff..." type="number" value="">'
-
-
-Install
-=======
-
-You can install **WTForms HTML5 Widgets** with pip_ or from source.
-
-Install with pip
-----------------
-
-pip_ is "*a tool for installing and managing Python packages*". If you don't
-have it installed, see the `pip install instructions`_.
-
-  ``pip install wtforms-html5``
-
-Install from source
--------------------
-
-You can fetch the latest sourceball_ from github and unpack it, or just clone
-this repository: ``git clone git://github.com/brutus/wtforms-html5.git``. If you
-got the source, change into the directory and use ``setup.py``:
-
-  ``python setup.py install``
-
-Since **WTForms HTML5 Widgets** only adds functionallity to WTForms_, you need
-to have WTForms_ installed too. But if you use the installation methods
-described above, it should have been taken care of.
-
-
-Details
-=======
-
-Supported INPUT types
----------------------
-
-* TextField
-* SearchField
-* URLField
-* EmailField
-* TelField
-* IntegerField
-* IntegerRangeField
-* DecimalField
-* DecimalRangeField
-* FloatField
-* FloatRangeField
-* DateField
-
-
-Supported auto-attributes
--------------------------
-
-* **title**
-
-  If no *title* attribute is provided for a field, but a *description*,
-  the description is used for the *title*.
-
-* **required**
-
-  Is set if the field has the ``required`` flag set.
-
-  This happens if you use one of these validators: *DataRequired* or
-  *InputRequired*. Or *DataNotNone* from this modul. You can set just the flag
-  without any validator logic with the *Required* validator from this modul.
-
-* **min** and **max**
-
-  If either *Length*, *NumberRange* or *DateRange* is used as a
-  validator and sets a minimal or maximal value, the corresponding INPUT
-  attribute is set.
-
-* **invalid**
-
-  If the field got any validation errors, the css class *invalid* gets set.
-
-
-New validators
---------------
-
-* **Required**
-
-  The *Required* validator from wtforms is an old alias for *DataRequired*.
-  It is deprecated and will be removed in 1.2. The *Required* validator from
-  this modul just sets the ``required`` flag, without any validator logic.
-
-* **DataNotNone**
-
-  Works like the original *DataRequired* but only raises an Error if the
-  data is ``None``, so that ``False`` or ``0`` are accepted values.
-
-* **DateRange**
-
-  Allows the use of *min* and *max* limits for date fields.
-
-
-Testing and Contribution
-========================
-
-**WTForms HTML5 Widgets** is at home at: https://github.com/brutus/wtforms-html5/
-
-If you find any bugs, issues or anything, please use the `issue tracker`_.
-
-Testing
--------
-
-There are some **doctest** in the module. You can either run them from the
-*source directory* like this ``python wtforms_html5.py -v`` or, if you got
-this modul already installed, like this ``python -m doctest -v
-wtforms_html5``.
-
-If you want to run the **test cases**, see that you got nose_ installed. You
-can install it like this: ``pip install nose``. Now either run ``nosetests``
-from the *source directory* or, if you got this modul already installed, run
-them like this: ``nosetest test_wtforms_html5``.
-
-If something fails, please get in touch.
-
-
-.. _home: https://github.com/brutus/wtforms-html5/
-.. _sourceball: https://github.com/brutus/wtforms-html5/zipball/master
-.. _`issue tracker`: https://github.com/brutus/wtforms-html5/issues
-.. _WTForms: http://wtforms.simplecodes.com/
-.. _pip: http://www.pip-installer.org/en/latest/index.html
-.. _`pip install instructions`: http://www.pip-installer.org/en/latest/installing.html
-.. _nose: http://readthedocs.org/docs/nose/en/latest/testing.html
-
-"""
-
-from wtforms import TextField as _TextField
-from wtforms import IntegerField as _IntegerField
-from wtforms import DecimalField as _DecimalField
-from wtforms import FloatField as _FloatField
-from wtforms import DateField as _DateField
-
-from wtforms.widgets import Input as _Input
-
-from wtforms.validators import Length, NumberRange, StopValidation
-
-from wtforms.compat import string_types
-
-
-__all__ = (
-  # WIDGETS
-  'Input',
-  'TextInput',
-  'DateInput',
-  'URLInput',
-  'EmailInput',
-  'SearchInput',
-  'TelInput',
-  'NumberInput',
-  'RangeInput',
-  'DecimalInput',
-  'DecimalRangeInput',
-  # FIELDS
-  'TextField',
-  'SearchField',
-  'URLField',
-  'EmailField',
-  'TelField',
-  'IntegerField',
-  'DateField',
-  'DecimalField',
-  'FloatField',
-  'IntegerRangeField',
-  'DecimalRangeField',
-  'FloatRangeField',
-  # VALIDATORS
-  'Required',
-  'DataNotNone',
-  'DateRange'
+from wtforms.fields.core import UnboundField
+from wtforms.meta import DefaultMeta
+from wtforms.validators import (
+    Length,
+    NumberRange,
 )
 
 
-# CUSTOM LOGIC
+MINMAX_VALIDATORS = (
+    Length,
+    NumberRange,
+)
 
-def get_html5_kwargs(field, kwargs):
-  """
-  Return new *kwargs* for *field*.
 
-  If the field got a *description* but no *title* is set, the *title*
-  is set to *description*.
+def get_html5_kwargs(field, render_kw=None):
+    """
+    Returns a copy of *render_kw*  with keys added for a bound *field*.
 
-  Generate *kwargs* for the new HTML5 INPUT attributes (`required`, `min` and
-  `max`) based on the validators set for *field*.
+    If some *render_kw* are given, the new keys are added to a copy of them,
+    which is then returned. If none are given, a dictionary containing only the automatically generated keys is returned.
 
-  Also set (or append) 'invalid' to the fields class(es), if the *field* got
-  errors. 'invalid' is also set by browsers if they detect errors on a field.
+    .. important::
 
-  If a key is already in *kwargs* it is left untouched.
+        This might add keys but never changes any values. If a key already is
+        in *render_kw* it is left untouched.
 
-  """
-  # got description?
-  if not 'title' in kwargs and getattr(field, 'description'):
-    kwargs['title'] = '{}'.format(field.description)
-  # is field required?
-  if not 'required' in kwargs and field.flags.required:
-    kwargs['required'] = 'required'
-  # check validators Length, NumberRange or DateRange
-  for vali in field.validators:
-    if (
-      isinstance(vali, Length) or
-      isinstance(vali, NumberRange) or
-      isinstance(vali, DateRange)
-    ):
-      if not 'min' in kwargs and hasattr(vali, 'min'):
-        if vali.min and vali.min != -1:
-          kwargs['min'] = vali.min
-      if not 'max' in kwargs and hasattr(vali, 'max'):
-        if vali.max and vali.max != -1:
-          kwargs['max'] = vali.max
-  # check for errors
-  if field.errors:
-    cls = kwargs.get('class', kwargs.pop('class_', ''))
-    if cls:
-      kwargs['class'] = 'invalid {}'.format(cls)
-    else:
-      kwargs['class'] = 'invalid'
-  return kwargs
+    Raises:
 
+        ValueError: if *field* is an :cls:`UnboundField`.
 
-# WIDGETS
+    The following keys are set automatically:
 
-class Input(_Input):
-  """Base INPUT class. Subclass this."""
+    :required:
+        Sets the *required* key if the `required` flag is set for the
+        field (mostly the case if it is set by validators). This is
+        used by browsers to indicate a required field.
 
-  def __call__(self, field, **kwargs):
-    kwargs = get_html5_kwargs(field, kwargs)
-    return _Input.__call__(self, field, **kwargs)
+    :invalid:
+        Set (or appends) 'invalid' to the fields CSS class(es), if the *field*
+        got any errors. 'invalid' is also set by browsers if they detect
+        errors on a field.
 
+    :min / max:
+        Sets *min* and / or *max* keys if the `Length` or `NumberRange`
+        validator is using them.
 
-class TextInput(Input):
-  """Creates `<input type=text>` widget with custom *kwargs* parsing."""
-  input_type = "text"
+    :title:
+        If the field got a *description* but no *title* key is set, the
+        *title* is set to *description*.
 
+    """
+    if isinstance(field, UnboundField):
+        msg = 'This function needs a bound field not: {}'
+        raise ValueError(msg.format(field))
 
-class NumberInput(Input):
-  """Creates `<input type=number>` widget with custom *kwargs* parsing."""
-  input_type = "number"
+    kwargs = render_kw.copy() if render_kw else {}
 
+    # is field required?
+    if 'required' not in kwargs and field.flags.required:
+        kwargs['required'] = True
 
-class DateInput(Input):
-  """Creates `<input type=date>` widget with custom *kwargs* parsing."""
-  input_type = "date"
+    # is field invalid?
+    if field.errors:
+        cls = kwargs.get('class') or kwargs.pop('class_', '')
+        if cls:
+            kwargs['class'] = 'invalid {}'.format(cls)
+        else:
+            kwargs['class'] = 'invalid'
 
+    # check validators for min/max
+    for validator in field.validators:
+        if isinstance(validator, MINMAX_VALIDATORS):
+            if 'min' not in kwargs:
+                v_min = getattr(validator, 'min', -1)
+                if v_min not in (-1, None):
+                    kwargs['min'] = v_min
+            if 'max' not in kwargs:
+                v_max = getattr(validator, 'max', -1)
+                if v_max not in (-1, None):
+                    kwargs['max'] = v_max
 
-class RangeInput(NumberInput):
-  """Creates `<input type=range>` widget with custom *kwargs* parsing."""
-  input_type = "range"
+    # missing tile?
+    if 'title' not in kwargs and getattr(field, 'description'):
+        kwargs['title'] = '{}'.format(field.description)
 
+    return kwargs
 
-class URLInput(Input):
-  """Creates `<input type=url>` widget with custom *kwargs* parsing."""
-  input_type = "url"
 
+class AutoAttrMeta(DefaultMeta):
 
-class EmailInput(Input):
-  """Creates `<input type=email>` widget with custom *kwargs* parsing."""
-  input_type = "email"
+    """
+    Meta class WTForms :cls:`Form` classes.
 
+    It uses :func:`get_html5_kwargs` to automatically add some render
+    keywords for each field widget when it gets rendered.
 
-class SearchInput(Input):
-  """Creates `<input type=search>` widget with custom *kwargs* parsing."""
-  input_type = "search"
+    """
 
+    def render_field(self, field, render_kw):
+        """
+        Returns the rendered field after adding auto–attributes.
 
-class TelInput(Input):
-  """Creates `<input type=tel>` widget with custom *kwargs* parsing."""
-  input_type = "tel"
+        Calls the field`s widget with the following kwargs:
 
+        1. the *render_kw* set on the field are used as based
+        2. and are updated with the *render_kw* arguments from the render call
+        3. this is used as an argument for a call to `get_html5_kwargs`
+        4. the return value of the call is used as final *render_kw*
 
-class DecimalInput(NumberInput):
-
-  """Creates `<input type=number>` widget with custom *kwargs* parsing.
-
-  Also adds a *step size* of ``any`` to the field, if not other step
-  size is set.
-
-  """
-
-  def __call__(self, field, **kwargs):
-    if not 'step' in kwargs:
-      kwargs['step'] = 'any'
-    return NumberInput.__call__(self, field, **kwargs)
-
-
-class DecimalRangeInput(DecimalInput):
-
-  """Creates `<input type=range>` widget with custom *kwargs* parsing.
-
-  Also inherits *step size* from ``DecimalInput``.
-
-  """
-
-  input_type = "range"
-
-
-# FIELDS
-
-class TextField(_TextField):
-  """**TextField** using **TextInput** by default """
-  widget = TextInput()
-
-
-class SearchField(TextField):
-  """**TextField** using **SearchInput** by default """
-  widget = SearchInput()
-
-
-class URLField(TextField):
-  """**TextField** using **URLInput** by default """
-  widget = URLInput()
-
-
-class EmailField(TextField):
-  """**TextField** using **EmailInput** by default """
-  widget = EmailInput()
-
-
-class TelField(TextField):
-  """**TextField** using **TelInput** by default """
-  widget = TelInput()
-
-
-class IntegerField(_IntegerField):
-  """**IntegerField** using **NumberInput** by default """
-  widget = NumberInput()
-
-
-class DateField(_DateField):
-  """**DateField** using **DateInput** by default """
-  widget = DateInput()
-
-
-class DecimalField(_DecimalField):
-  """**DecimalField** using **DecimalInput** by default """
-  widget = DecimalInput()
-
-
-class FloatField(_FloatField):
-  """**DecimalField** using **DecimalInput** by default """
-  widget = DecimalInput()
-
-
-class IntegerRangeField(_IntegerField):
-  """**IntegerField** using **RangeInput** by default """
-  widget = RangeInput()
-
-
-class DecimalRangeField(_DecimalField):
-  """**DecimalField** using **DecimalRangeInput** by default """
-  widget = DecimalRangeInput()
-
-
-class FloatRangeField(_FloatField):
-  """**DecimalField** using **DecimalRangeInput** by default """
-  widget = DecimalRangeInput()
-
-
-# VALIDATORS
-
-
-class Required(object):
-
-  """
-  No validation, just set the *required* flag.
-
-  Setting the *required* flag along with ``get_html5_kwargs`` prevents
-  browsers from sending empty fields.
-
-  """
-
-  field_flags = ('required', )
-
-  def __call__(self, form, field):
-    pass
-
-
-class DataNotNone(object):
-
-  """
-  Validates that the field contains data that is not ``None``.
-
-  This validator will stop the validation chain on error.
-
-  If the data is not ``None``, it also removes prior errors (such as
-  processing errors) from the field.
-
-  :param message:
-      Error message to raise in case of a validation error.
-
-  """
-
-  field_flags = ('required', )
-
-  def __init__(self, message=None):
-      self.message = message
-
-  def __call__(self, form, field):
-    if field.data is None or (
-      isinstance(field.data, string_types)
-      and not field.data.strip()
-    ):
-      if self.message is None:
-        self.message = field.gettext('This field is required.')
-      field.errors[:] = []
-      raise StopValidation(self.message)
-
-
-class DateRange(object):
-
-  """
-  Validate that a date is in the *min* and *max* range.
-
-  :param message:
-    Error message to raise in case of a validation error.
-    *message* should be a tuple if you want two different error messages.
-
-  """
-
-  def __init__(self, min=None, max=None, message=None):
-    try:
-      self.err_min, self.err_max = message
-    except TypeError:
-      self.err_min = self.err_max = message
-    self.min = min
-    self.max = max
-
-  def __call__(self, form, field):
-    if self.min and field.data < self.min:
-      if not self.err_min:
-        self.err_min = field.gettext("Date must be >= {}.").format(self.min)
-      field.errors.append(self.err_min)
-    if self.max and field.data > self.max:
-      if not self.err_max:
-        self.err_max = field.gettext("Date must be >= {}.").format(self.max)
-      field.errors.append(self.err_max)
-
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
+        """
+        field_kw = getattr(field, 'render_kw', None)
+        if field_kw is not None:
+            render_kw = dict(field_kw, **render_kw)
+        render_kw = get_html5_kwargs(field, render_kw)
+        return field.widget(field, **render_kw)
