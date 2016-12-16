3c9ac6d2e465ea1f385d2489fd1457d15311ca38
Brutus <brutus.dmc@googlemail.com>
2016-12-16 20:54:45 +0100

    added functions for each auto setting

--- wtforms_html5.py
+++ wtforms_html5.py
@@ -4,7 +4,7 @@
 Generates render keywords for `WTForms`_ HTML5 field's widgets.
 
 The :func:`get_html5_kwargs` adds the automatically generated keys to the
-_render keywords_ of a `WTForms`_ field.
+*render keywords* of a `WTForms`_ field.
 
 The :cls:`AutoAttrMeta` can be included as a base class for the `Meta` class
 in your forms, to handle this automatically for each field of the form.
@@ -13,36 +13,43 @@ in your forms, to handle this automatically for each field of the form.
 Supported Auto–Attributes
 -------------------------
 
-- **required**
+- *required*
 
-  Is set if the field has the ``required`` flag set.
+  Is set if the field has the _required_ flag set. This happens i.e. if you
+  use the _DataRequired_ or _InputRequired_ validator. The `required`
+  attribute is used by browsers to indicate a required field (and most
+  browsers won't activate the forms action unless all required fields have
+  content).
 
-  This happens if you use the *DataRequired* or *InputRequired* validator.
+- *invalid*
 
-- **invalid**
+  If the field got any validation errors, the CSS class _invalid_ is added.
+  The `invalid` class is also set by browsers if they detect errors on a
+  field. This validation errors detected by your code, are by default styled
+  in the same way as browser generated errors.
 
-  If the field got any validation errors, the CSS class *invalid* is added.
+- *min* and *max*
 
-- **min** and **max**
+  If either _Length_ or _NumberRange_ is used as a validator to set minimal
+  and / or maximal values, the corresponding `min` / `max` INPUT attribute is
+  set. This allows for browser based validation of the values.
 
-  If either *Length* or *NumberRange* is used as a validator and sets a minimal
-  or maximal value, the corresponding INPUT attribute is set.
+- *title*
 
-- **title**
-
-  If no *title* attribute is provided for a field, but a *description*, the
-  *description* is used for the *title*.
+  If no _title_ is provided for a field, the _description_ (if one is set) is
+  used for the `title` attribute.
 
 
-Example
--------
+An Example
+----------
 
-Declare your form just like in vanilla *WTForms*, but include
-:cls:`AutoAttrMeta` as your meta class:
+Declare your form just like in vanilla *WTForms*, but include `AutoAttrMeta`
+as your meta class:
 
 >>> from wtforms import Form, StringField
 >>> from wtforms.validators import InputRequired, Length
 >>> from wtforms_html5 import AutoAttrMeta
+
 >>> class MyForm(Form):
 ...   class Meta(AutoAttrMeta):
 ...     pass
@@ -51,30 +58,40 @@ Declare your form just like in vanilla *WTForms*, but include
 ...      validators=[InputRequired(), Length(min=3, max=12)],
 ...      description='Just a test field.',
 ...   )
+
 >>> form = MyForm()
 
 **The only difference is, that you include a `Meta` class, that inherits from
-:cls:`AutoAttrMeta`.**
+`AutoAttrMeta`.**
 
 This meta class sets the above mentioned attributes automatically for all the
 fields of the form:
 
->>> form.test_field()
-'<input id="test_field" max="12" min="3" name="test_field" required \
-title="Just a test field." type="text" value="">'
+>>> f = form.test_field()
+>>> exp = (
+...  '<input id="test_field" max="12" min="3" name="test_field" required '
+...  'title="Just a test field." type="text" value="">'
+... )
+>>> assert f == exp
 
-The *min* and *max* attributes are created because the `Length` validator was
-used. And the field is marked *required* because of the `InputRequired`
-validator. The field also gets a *title* taken from the fields `description`.
+
+
+The _min_ and _max_ attributes are created because the `Length` validator was
+used. And the field is marked _required_ because of the `InputRequired`
+validator. The field also gets a _title_ taken from the fields `description`.
 
 If you validate the form and any errors pop up, the field also get `invalid`
 added to its class:
 
 >>> form.validate()
 False
->>> form.test_field()
-'<input class="invalid" id="test_field" max="12" min="3" name="test_field" \
-required title="Just a test field." type="text" value="">'
+>>> exp = (
+... '<input class="invalid" id="test_field" max="12" min="3" '
+... 'name="test_field" required title="Just a test field." type="text" '
+... 'value="">'
+... )
+>>> f = form.test_field()
+>>> assert f == exp
 
 
 .. _WTForms: https://wtforms.readthedocs.io/
@@ -104,7 +121,91 @@ MINMAX_VALIDATORS = (
 )
 
 
-def get_html5_kwargs(field, render_kw=None):
+def set_required(field, render_kw=None, force=False):
+    """
+    Returns *render_kw* with *required* set if the field is required.
+
+    Sets the *required* key if the `required` flag is set for the field (this
+    is mostly the case if it is set by validators). The `required` attribute
+    is used by browsers to indicate a required field.
+
+    ..note::
+
+        This won't change keys already present unless *force* is used.
+
+    """
+    if render_kw is None:
+        render_kw = {}
+    if 'required' in render_kw and not force:
+        return render_kw
+    if field.flags.required:
+        render_kw['required'] = True
+    return render_kw
+
+
+def set_invalid(field, render_kw=None):
+    """
+    Returns *render_kw* with `invalid` added to *class* on validation errors.
+
+    Set (or appends) 'invalid' to the fields CSS class(es), if the *field* got
+    any errors. 'invalid' is also set by browsers if they detect errors on a
+    field.
+
+    """
+    if render_kw is None:
+        render_kw = {}
+    if field.errors:
+        classes = render_kw.get('class') or render_kw.pop('class_', '')
+        if classes:
+            render_kw['class'] = 'invalid {}'.format(classes)
+        else:
+            render_kw['class'] = 'invalid'
+    return render_kw
+
+
+def set_minmax(field, render_kw=None, force=False):
+    """
+    Returns *render_kw* with *min* and *max* set if validators use them.
+
+    Sets *min* and / or *max* keys if a `Length` or `NumberRange` validator is
+    using them.
+
+    ..note::
+
+        This won't change keys already present unless *force* is used.
+
+    """
+    if render_kw is None:
+        render_kw = {}
+    for validator in field.validators:
+        if isinstance(validator, MINMAX_VALIDATORS):
+            if 'min' not in render_kw or force:
+                v_min = getattr(validator, 'min', -1)
+                if v_min not in (-1, None):
+                    render_kw['min'] = v_min
+            if 'max' not in render_kw or force:
+                v_max = getattr(validator, 'max', -1)
+                if v_max not in (-1, None):
+                    render_kw['max'] = v_max
+    return render_kw
+
+
+def set_title(field, render_kw=None):
+    """
+    Returns *render_kw* with *min* and *max* set if required.
+
+    If the field got a *description* but no *title* key is set, the *title* is
+    set to *description*.
+
+    """
+    if render_kw is None:
+        render_kw = {}
+    if 'title' not in render_kw and getattr(field, 'description'):
+        render_kw['title'] = '{}'.format(field.description)
+    return render_kw
+
+
+def get_html5_kwargs(field, render_kw=None, force=False):
     """
     Returns a copy of *render_kw*  with keys added for a bound *field*.
 
@@ -114,8 +215,8 @@ def get_html5_kwargs(field, render_kw=None):
 
     .. important::
 
-        This might add keys but never changes any values. If a key already is
-        in *render_kw* it is left untouched.
+        This might add new keys but won't changes any values if a key is
+        already in *render_kw*,  unless *force* is used.
 
     Raises:
 
@@ -145,37 +246,11 @@ def get_html5_kwargs(field, render_kw=None):
     if isinstance(field, UnboundField):
         msg = 'This function needs a bound field not: {}'
         raise ValueError(msg.format(field))
-
     kwargs = render_kw.copy() if render_kw else {}
-
-    # is field required?
-    if 'required' not in kwargs and field.flags.required:
-        kwargs['required'] = True
-
-    # is field invalid?
-    if field.errors:
-        cls = kwargs.get('class') or kwargs.pop('class_', '')
-        if cls:
-            kwargs['class'] = 'invalid {}'.format(cls)
-        else:
-            kwargs['class'] = 'invalid'
-
-    # check validators for min/max
-    for validator in field.validators:
-        if isinstance(validator, MINMAX_VALIDATORS):
-            if 'min' not in kwargs:
-                v_min = getattr(validator, 'min', -1)
-                if v_min not in (-1, None):
-                    kwargs['min'] = v_min
-            if 'max' not in kwargs:
-                v_max = getattr(validator, 'max', -1)
-                if v_max not in (-1, None):
-                    kwargs['max'] = v_max
-
-    # missing tile?
-    if 'title' not in kwargs and getattr(field, 'description'):
-        kwargs['title'] = '{}'.format(field.description)
-
+    kwargs = set_required(field, kwargs, force)  # is field required?
+    kwargs = set_invalid(field, kwargs)  # is field invalid?
+    kwargs = set_minmax(field, kwargs, force)  # check validators for min/max
+    kwargs = set_title(field, kwargs)  # missing tile?
     return kwargs
 
 
